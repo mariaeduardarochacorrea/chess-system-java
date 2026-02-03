fabc8b606a062e125eb5eb4957655164157cce93
mariaeduardarochacorrea <mariaeduardarochacorrea@gmail.com>
2026-02-03 14:43:00 -0300

    First clas: Position

--- src/projeto_xadrez_novo/Program.java
+++ src/projeto_xadrez_novo/Program.java
@@ -1,10 +1,10 @@
 package projeto_xadrez_novo;
-
+import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
-
 }
