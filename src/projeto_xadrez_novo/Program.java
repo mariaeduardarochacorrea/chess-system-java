e2dc9d4a2f0744cb3c5685739fb93ba608b5e283
mariaeduardarochacorrea <mariaeduardarochacorrea@gmail.com>
2026-02-11 15:34:41 -0300

    Chess layer and printing the board

--- src/projeto_xadrez_novo/Program.java
+++ src/projeto_xadrez_novo/Program.java
@@ -1,10 +1,14 @@
 package projeto_xadrez_novo;
-import boardgame.Position;
+
+
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 }
