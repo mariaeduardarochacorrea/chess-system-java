c23c474f9d60a272d3684dc28f29ec9c49dbd263
mariaeduardarochacorrea <mariaeduardarochacorrea@gmail.com>
2026-02-22 15:44:31 -0300

    Handling captured pieces

--- src/projeto_xadrez_novo/Program.java
+++ src/projeto_xadrez_novo/Program.java
@@ -2,8 +2,9 @@ package projeto_xadrez_novo;
 
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -15,11 +16,14 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		
 		
 		while (true) {
 			try {
 			    UI.clearScreen();
-			    UI.printMatch(chessMatch);
+			    UI.printMatch(chessMatch, captured);
 				System.out.println();
 			    System.out.print("Source: ");
 			    ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +36,10 @@ public class Program {
 			    ChessPosition target = UI.readChessPosition(sc);
 			   
 			    ChessPiece capturedPiece = chessMatch.performChessMove(source, target); 
+			    
+			    if (capturedPiece != null) {
+			    	captured.add(capturedPiece);
+			    }
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
